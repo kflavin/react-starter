292bf631986542d85e2428db1a9827be14129fea
Kyle <Kyle.Flavin@gmail.com>
2021-01-18 17:28:58 -0800

    added react-axe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App'
 import './styles.css'
 import DefaultErrorBoundary from './DefaultErrorBoundary'
 
+// NODE_ENV is available during the build
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000) // 1000 ms delay
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundary>
