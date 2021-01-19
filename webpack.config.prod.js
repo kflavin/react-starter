da48e454ab748ba434ce7ce26032ebb36aa0ebae
Kyle <Kyle.Flavin@gmail.com>
2021-01-18 16:02:29 -0800

    working test

---
+++ webpack.config.prod.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const baseConfig = require('./webpack.config.base.js')
+
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  plugins: [new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false
+  })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+})
